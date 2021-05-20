c8fc7527e0c2afeea86734b5588764e80db01356
Katkoff <katkoff@mail.ru>
2021-05-20 19:41:10 +0500

    remove folder

--- products/simpleapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm, BooleanField
-from .models import Product
-
-
-class ProductForm(ModelForm):
-    #check_box = BooleanField(label = 'Алло, Галочка')
-    class Meta:
-        model = Product
-        fields = ['name', 'description', 'quantity', 'category', 'price']
