29930c507a163361205f07a80c11b0abfb2945ee
Katkoff <katkoff@mail.ru>
2021-05-20 19:37:59 +0500

    remove folder

--- skillfactory_mcdonalds-main/board/.ipynb_checkpoints/tasks-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-from .models import Order
-
-@shared_task
-def complete_order(oid):
-    order = Order.objects.get(pk = oid)
-    order.complete = True
-    order.save()
