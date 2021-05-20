d5eeb97ba5c6929d5eb85bfde3cd6540699ed040
Katkoff <katkoff@mail.ru>
2021-05-20 19:43:26 +0500

    remove folder

--- np_mod_04/np/news/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, BooleanField
-from .models import Post
-
-
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['to_author', 'category_post', 'header_post', 'content_post']
