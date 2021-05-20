d53ca033da4b5007efe84dd9deeaf1e61ee208a1
Katkoff <katkoff@mail.ru>
2021-05-20 19:43:56 +0500

    remove folder

--- np_mod_06/np/appointment/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Appointment
-
-
-class MakeForm(ModelForm):
-    class Meta:
-        model = Appointment
-        fields = ['date', 'client_name', 'message']
--- np_mod_06/np/np/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'np.settings')
-
-app = Celery('np')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
